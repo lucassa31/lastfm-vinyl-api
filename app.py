b73ef987a4511388f130e47c4faf0695c09d544f
lucassa31 <lucas.staguet@gmail.com>
2025-08-15 23:31:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,14 @@ def convert_and_stream():
     except Exception:
         return "Erreur lors de l'ouverture de l'image", 400
 
-    img = img.resize((240, 240)).convert('RGB565')
+    # L'image est d'abord convertie en RGB
+    img = img.convert('RGB')
+    
+    # Puis convertie en RGB565 en spécifiant le dither
+    img = img.convert('RGB565', dither=Image.NONE) 
+
+    img = img.resize((240, 240)) # La conversion de taille doit être faite à la fin
+
     output_stream = io.BytesIO(img.tobytes())
     output_stream.seek(0)
 
