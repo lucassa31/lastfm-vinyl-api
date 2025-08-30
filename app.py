b3f98a2f1b466ad60a807b4ecaa9134abf504ea1
lucassa31 <lucas.staguet@gmail.com>
2025-08-30 22:21:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,11 @@ from PIL import Image
 import requests
 import io
 import struct
-import textwrap
 
 app = Flask(__name__)
 
-@app.route('/convert_and_generate_py', methods=['GET'])
-def convert_and_generate_py():
+@app.route('/convert_and_generate', methods=['GET'])
+def convert_and_generate():
     image_url = request.args.get('url')
     if not image_url:
         return "URL d'image manquante", 400
@@ -31,39 +30,16 @@ def convert_and_generate_py():
         g = rgb_data[i*3 + 1]
         b = rgb_data[i*3 + 2]
         
+        # Le format LVGL par défaut est Little-Endian
         rgb565 = ((r >> 3) << 11) | ((g >> 2) << 5) | (b >> 3)
         lv_bytes.extend(struct.pack('<H', rgb565))
 
-    # Convertit les octets en une chaîne hexadécimale échappée
-    hex_str = ''.join(f'\\x{b:02X}' for b in lv_bytes)
-
-    # Découpe la chaîne en lignes de 128 caractères et ajoute la syntaxe \
-    line_length = 128
-    wrapped_lines = textwrap.wrap(hex_str, width=line_length)
-    wrapped_str = '\\\n'.join(wrapped_lines)
-
-    py_code = f"""import lvgl as lv
-
-TemporaryImage_data = b'{wrapped_str}'
-
-TemporaryImage = lv.img_dsc_t({{
-    'header': {{
-        'always_zero': 0,
-        'w': {width},
-        'h': {height},
-        'cf': lv.img.CF.TRUE_COLOR
-    }},
-    'data_size': len(TemporaryImage_data),
-    'data': TemporaryImage_data
-}})
-"""
-
+    # Retourne les données binaires brutes directement
     return Response(
-        py_code,
-        mimetype="text/x-python",
-        headers={"Content-Disposition": "attachment; filename=album_art.py"}
+        lv_bytes,
+        mimetype="application/octet-stream",
+        headers={"Content-Disposition": "attachment; filename=album_art.bin"}
     )
 
 if __name__ == '__main__':
     app.run()
-
